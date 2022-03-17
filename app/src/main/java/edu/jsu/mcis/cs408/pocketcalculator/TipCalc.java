be9d607e6be68d253e4a88bc3598b62f4796c852
nkey0011 <nkey@stu.jsu.edu>
2022-03-17 13:52:17 -0500

    cleanup

--- app/src/main/java/edu/jsu/mcis/cs408/pocketcalculator/TipCalc.java
+++ app/src/main/java/edu/jsu/mcis/cs408/pocketcalculator/TipCalc.java
@@ -8,6 +8,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+/*
+tip calculator code
+public void onClick(View v) {
+
+                // takes user's total bill input and converts to tangible number
+                bill = binding.billInput;
+                String user_bill = bill.getText().toString();
+                float amt_bill = Float.parseFloat(user_bill);
+
+                // takes user's tip input as whole number
+                tip = binding.tipInput;
+                String user_tip = tip.getText().toString();
+                float amt_tip = Float.parseFloat(user_tip);
+
+                // takes user's people input
+                people = binding.peopleInput;
+                String user_people = people.getText().toString();
+                int amt_people = Integer.parseInt(user_people);
+
+                // converts tip input into a decimal amount
+                float tip_value = amt_tip / 100;
+
+                /* adds tip to the final bill and divides by amount of people
+                to get the price for each person
+                double final_bill = (amt_bill + tip_value)/amt_people;
+
+                        NumberFormat money_format = NumberFormat.getCurrencyInstance();
+                        String output = money_format.format(final_bill);
+
+                        binding.result.setText("Price Per Person: " + output);
+                        }
+ */
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TipCalc#newInstance} factory method to
